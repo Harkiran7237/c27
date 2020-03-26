5f11e2ce6be1b5cc2c8273b22fdeab33354883c8
harkiran7237 <harkiran7237@gmail.com>
2020-03-26 15:17:05 +0530

    classs

---
+++ chain.js
@@ -0,0 +1,18 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var options={
+        bodyA:bodyA,
+        bodyB: bodyB,
+        stiffness: 0.04,
+        length:10
+    }
+    
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+    }
+    display(){
+        var pointA=this.chain.position.x;
+        var pointB=this.chain.position.y;
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
